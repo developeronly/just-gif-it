8c95776545dab2b580fc204466c20acecf83929a
developeronly <developeronly@gmail.com>
2017-03-20 19:35:50 +0530

    Excluding configurations which are not required.

--- src/main/java/com/example/JustGifItApplication.java
+++ src/main/java/com/example/JustGifItApplication.java
@@ -1,12 +1,73 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.HttpPutFormContentFilter;
+import org.springframework.web.filter.RequestContextFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
+import javax.annotation.PostConstruct;
+import java.io.File;
+
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class, JmxAutoConfiguration.class,
+        WebSocketAutoConfiguration.class})
 public class JustGifItApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JustGifItApplication.class, args);
-	}
+    @Value("${multipart.location}/gif/")
+    private String gifLocation;
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustGifItApplication.class, args);
+    }
+
+    @PostConstruct
+    private void init() {
+        File gifFolder = new File(gifLocation);
+        if (!gifFolder.exists()) {
+            gifFolder.mkdir();
+        }
+    }
+
+    @Bean
+    public FilterRegistrationBean deRegisterHiddenHttpMethodFilter(HiddenHttpMethodFilter hiddenHttpMethodFilter) {
+        FilterRegistrationBean bean = new FilterRegistrationBean(hiddenHttpMethodFilter);
+        bean.setEnabled(false);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean deRegisterHttpPutFormContentFilter(HttpPutFormContentFilter filter) {
+        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+        bean.setEnabled(false);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean deRegisterRequestContextFilter(RequestContextFilter filter) {
+        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+        bean.setEnabled(false);
+        return bean;
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/gif/**")
+                        .addResourceLocations("file:" + gifLocation);
+                super.addResourceHandlers(registry);
+            }
+        };
+    }
+
 }
